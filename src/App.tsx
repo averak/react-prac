e74d2bb09c1851a912f6372227ad8616dc20a065
averak <abe12@mccc.jp>
2020-11-07 22:14:15 +0900

    [disable] デフォルトの表示を無効化

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  let title: string = "テストタイトル";
+  let message: string = "テストメッセージ";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>{title}</h2>
+      <p>{message}</p>
     </div>
   );
 }
